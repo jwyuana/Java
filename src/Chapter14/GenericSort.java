8a6165893317ff36fa872eb9dda5d9d0d6776584
jwyuana <jwyuan2004@gmail.com>
2014-07-07 09:19:55 -0500

    sort

---
+++ src/Chapter14/GenericSort.java
@@ -0,0 +1,47 @@
+package Chapter14;
+
+public class GenericSort {
+
+	public static void main(String[] args){
+		Integer[] intArray = {new Integer(2), new Integer(4), new Integer(3)};
+		Double[] doubleArray = {new Double(3.4), new Double(1.3), new Double(-22.1)};
+		Character[] charArray = {new Character('a'), new Character('J'), new Character('r')};
+		String[] stringArray = {"Tom", "John", "Fred"};
+		
+		sort(intArray);
+		sort(doubleArray);
+		sort(charArray);
+		sort(stringArray);
+		
+		printList(intArray);
+		printList(doubleArray);
+		printList(charArray);
+		printList(stringArray);	
+	}
+	
+	public static void sort(Comparable[] list){
+		Comparable currentMin;
+		int currentMinIndex;
+		
+		for(int i=0; i<list.length-1; i++){
+			currentMin = list[i];
+			currentMinIndex = i;
+			for(int j=i+1; j<list.length; j++)
+				if(currentMin.compareTo(list[j]) > 0){
+					currentMin = list[j];
+					currentMinIndex = j;
+				}
+			if(currentMinIndex != i){
+				list[currentMinIndex] = list[i];
+				list[i] = currentMin;
+			}
+		}
+	}
+	
+	public static void printList(Object[] list){
+		for(int i=0; i<list.length; i++)
+			System.out.print(list[i] + " ");
+		System.out.println();
+	}
+	
+}
